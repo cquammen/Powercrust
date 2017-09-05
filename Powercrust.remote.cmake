4d2933bc078ea6256467837bdbcf81e1cb9de891
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-05 12:39:42 -0400

    ENH: First attemp at a remote modue

---
+++ Powercrust.remote.cmake
@@ -0,0 +1,10 @@
+#
+# Powercrust Reconstruction
+#
+
+vtk_fetch_module(Powercrust
+  "Powercrust Surface reconstruction from unorganized points"
+  GIT_REPOSITORY https://github.com/lorensen/Powercrust
+  # September 2017  - first working as a remote module
+  GIT_TAG master
+  )
