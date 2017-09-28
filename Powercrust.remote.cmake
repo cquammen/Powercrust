2cd346ca916ed73ebe0708eb7f648c0904df015f
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-28 07:48:51 -0400

    ENH: Bump version

--- Powercrust.remote.cmake
+++ Powercrust.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(Powercrust
   "Powercrust Surface reconstruction from unorganized points"
   GIT_REPOSITORY https://github.com/lorensen/Powercrust
-  # September 9, 2017  - first working as a remote module
-  GIT_TAG 2c05083036a10a69812ccb0ac180897256e25268
+  # September 28, 2017  - first working as a remote module
+  GIT_TAG deff33c2fdcb91c9db23cf0319b0ad5fc4f433ea
   )
